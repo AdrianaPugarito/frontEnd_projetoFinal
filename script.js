f471399c8a9c6da2786b4bcac0a14227d94b191e
AdrianaPugarito <85944220+AdrianaPugarito@users.noreply.github.com>
2021-08-25 23:53:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+
+  $("#loginAluno").submit(function(){
+
+    var usuario = $("#inputUsuario").val()
+    var senha = $("#inputSenha").val()
+
+    if(usuario == "root". )
+
+
+
+
+
+
+  })
+
+
+})
